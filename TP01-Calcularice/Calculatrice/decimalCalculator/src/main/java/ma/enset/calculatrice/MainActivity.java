2786dfd6f9cb9155994a53ee95cd317b1b99a1a5
Aya Ait <aya.aitoulahyane@gmail.com>
2023-02-18 13:55:39 +0100

    Fixed orientation keep data

--- TP01-Calcularice/Calculatrice/decimalCalculator/src/main/java/ma/enset/calculatrice/MainActivity.java
+++ TP01-Calcularice/Calculatrice/decimalCalculator/src/main/java/ma/enset/calculatrice/MainActivity.java
@@ -1,8 +1,8 @@
 package ma.enset.calculatrice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MaterialButton buttonC, buttonBrackOpen, buttonBrackClose;
     MaterialButton buttonDiv, buttonMult, buttonPlus, buttonMin, buttonEq;
     MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonFac, buttonSqrt, buttonPow, buttonLog, buttonLn, buttonSin, buttonCos, buttonTan,buttonPi, buttonE, buttonAns, buttonInv, buttonRad,buttonDeg;
     MaterialButton buttonAC, buttonDot;
 
     @Override
@@ -54,8 +55,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             assignId(button7, R.id.button_7);
             assignId(button8, R.id.button_8);
             assignId(button9, R.id.button_9);
-        }
 
+            //Test if orientation changed
+            if (savedInstanceState != null) {
+                assignId(buttonSqrt, R.id.button_sqrt);
+                assignId(buttonFac, R.id.button_fac);
+                assignId(buttonPow, R.id.button_pow);
+                assignId(buttonLog, R.id.button_log);
+                assignId(buttonLn, R.id.button_ln);
+                assignId(buttonSin, R.id.button_sin);
+                assignId(buttonCos, R.id.button_cos);
+                assignId(buttonTan, R.id.button_tan);
+                assignId(buttonPi, R.id.button_pi);
+                assignId(buttonE, R.id.button_e);
+                assignId(buttonAns, R.id.button_ans);
+                assignId(buttonInv, R.id.button_inv);
+                assignId(buttonRad, R.id.button_rad);
+                assignId(buttonDeg, R.id.button_deg);
+            }
+        }
+        if (savedInstanceState != null) {
+            String solution = savedInstanceState.getString("solution");
+            solutionTv.setText(solution);
+            String result = savedInstanceState.getString("result");
+            resultTv.setText(result);
+            savedInstanceState.clear();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("solution", solutionTv.getText().toString());
+        outState.putString("result", resultTv.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        savedInstanceState.getString("solution");
+        savedInstanceState.getString("result");
     }
 
     void assignId(MaterialButton btn, int id) {
@@ -87,13 +126,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             dataToCalculate = dataToCalculate + buttonText;
         }
+        if(buttonText.equals("x^y")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+            dataToCalculate = dataToCalculate + "^";
+        }
+        if(buttonText.equals("x!")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 2);
+            dataToCalculate = dataToCalculate + "!";
+        }
+        if(buttonText.equals("sin")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 3);
+            dataToCalculate = dataToCalculate + "sin(";
+        }
+        if(buttonText.equals("cos")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 3);
+            dataToCalculate = dataToCalculate + "cos(";
+        }
+        if(buttonText.equals("tan")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 3);
+            dataToCalculate = dataToCalculate + "tan(";
+        }
+        if(buttonText.equals("log")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 3);
+            dataToCalculate = dataToCalculate + "log(";
+        }
+        if(buttonText.equals("ln")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 2);
+            dataToCalculate = dataToCalculate + "ln(";
+        }
+        if(buttonText.equals("√")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+            dataToCalculate = dataToCalculate + "sqrt(";
+        }
+        if(buttonText.equals("π")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+            dataToCalculate = dataToCalculate + "pi";
+        }
+        if(buttonText.equals("e")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+            dataToCalculate = dataToCalculate + "e";
+        }
+        if(buttonText.equals("Ans")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 3);
+            dataToCalculate = dataToCalculate + "ans";
+        }
+        if(buttonText.equals("deg")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 3);
+            dataToCalculate = dataToCalculate + "deg(";
+        }
+        if(buttonText.equals("Rad")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 3);
+            dataToCalculate = dataToCalculate + "rad(";
+        }
+
         solutionTv.setText(dataToCalculate);
+        System.out.println(dataToCalculate);
 
         String finalResult = getResult(dataToCalculate);
         if (!finalResult.equals("NaN")) {
             resultTv.setText(finalResult);
-        } else {
-            return;
         }
 
     }
@@ -101,6 +192,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String getResult(String data) {
         //boolean test = Pattern.matches("^[-+]?[0-9]*\\.?[0-9]+([-+*/]?([0-9]*\\.?[0-9]+))*$", data);
         try {
+            if (data.endsWith("+") || data.endsWith("-") || data.endsWith("*") || data.endsWith("/")) {
+                data = data.substring(0, data.length() - 1);
+            }
+            if (data.startsWith("*") || data.startsWith("/")) {
+                data = data.substring(1);
+            }
+            if (data.contains("++") || data.contains("--") || data.contains("**") || data.contains("//")) {
+                return "NaN";
+            }
+            if (data.contains("+-") || data.contains("-+") || data.contains("*-") || data.contains("/-")) {
+                data = data.replace("+-", "-");
+                data = data.replace("-+", "-");
+                data = data.replace("*-", "*-1*");
+                data = data.replace("/-", "/-1*");
+            }
+
             //TODO: mXparser
 /*
                 Context context = Context.enter();
